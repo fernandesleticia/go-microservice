38fd24a6af23620e6167e33284d98ec068af580d
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-06-03 19:38:20 -0300

    adding endpoints

---
+++ pkg/endpoints/reqJSONMap.go
@@ -0,0 +1,25 @@
+package endpoints
+
+type GetRequest struct {
+	Filters []internal.Filter `json:"filters,omitempty"`
+}
+
+type GetResponse struct {
+
+}
+
+type StatusRequest struct {
+
+}
+
+type StatusResponse struct {
+
+}
+
+type ServiceStatusRequest struct{
+
+}
+
+type ServiceStatusResponse struct {
+	
+}
