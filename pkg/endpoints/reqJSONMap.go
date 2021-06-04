75706fc9330c7529b9b432157e3bbc2603124044
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-06-04 20:58:17 -0300

    update req mapping

--- pkg/endpoints/reqJSONMap.go
+++ pkg/endpoints/reqJSONMap.go
@@ -5,21 +5,30 @@ type GetRequest struct {
 }
 
 type GetResponse struct {
-
+	Transactions []internal.Transaction `json:"transactions"`
+	Err 		 string 				`json:"err,omitempty"`
 }
 
 type StatusRequest struct {
-
+	TransactionID string `json:"status"`
 }
 
 type StatusResponse struct {
-
+	Status internal.Status `json:"status"`
+	Err string 			   `json:"err,omitempty"`
 }
 
-type ServiceStatusRequest struct{
-
+type TransactionRequest struct {
+	Transaction *internal.Transaction `json:"transaction"`
 }
+
+type TransactionResponse struct{
+	TransactionID string `json:"transactionID"`
+}
+
+type ServiceStatusRequest struct{}
 
 type ServiceStatusResponse struct {
-	
+	Code int   `json:"status"`
+	Err string `json:"err,omitempty"`
 }
