dcd2eb2f773bec3d2863a81b96cb53f8c07b1b8d
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-06-05 21:49:40 -0300

    add logger to endpoint

---
+++ pkg/endpoints/endpoints.go
@@ -0,0 +1,8 @@
+package endpoints
+
+var logger log.Logger
+
+func init() {
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+}
