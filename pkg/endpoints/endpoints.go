dfb9313630198a8ff74be8c9699ec69d9fe2f750
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-06-06 22:35:26 -0300

    getting status

--- pkg/endpoints/endpoints.go
+++ pkg/endpoints/endpoints.go
@@ -2,6 +2,18 @@ package endpoints
 
 var logger log.Logger
 
+func (s *Set) Status(ctx context.Context, transactionID string) (internal.Status, error) {
+	resp, err := s.StatusEndpoint(ctx, StatusRequest{TransactionID: transactionID})
+	if err != nil {
+		return internal.Failed, err
+	}
+	stsResp := resp.(StatusResponse)
+	if stsResp.Err != "" {
+		return internal.Failed, errors.New(stsResp.Err)
+	}
+	return stsResp.Status, nil
+}
+
 func init() {
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
